7804ef17ece0cd2fedce75086ad813242ca37396
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-25 17:08:42 +0930

    Set up router and controller to clean up router

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ const PORT = process.env.PORT || 5050
 const app = express()
 
 app.get("/", (req, res) => {
-  res.status(200).json({message: "Welcome to Hey Support API"})
+  res.status(200).json({ message: "Welcome to Hey Support API" })
 })
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
