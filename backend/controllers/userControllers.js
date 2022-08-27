83be75eef2d4d942a0450730152433ff74b96c05
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-27 19:42:44 +0930

    add ticket model/routes/controllers for backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,10 +3,9 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const User = require("../models/userModel")
-const { generatePrime } = require("crypto")
 
 // @desc    Register a new user
-// @route   /api/users
+// @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
@@ -50,7 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 // @desc    Login a user
-// @route   /api/users/login
+// @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
@@ -71,7 +70,7 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 // @desc    Get current user
-// @route   /api/users/me
+// @route   GET /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
   const user = {
