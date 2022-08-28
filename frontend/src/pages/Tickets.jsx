51f2d471e80f2ad9b8f60ec7da3bd80e68b2cfce
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-28 19:41:14 +0930

    display tickets in view

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,22 +3,45 @@ import { useSelector, useDispatch } from "react-redux"
 import { getTickets, reset } from "../features/tickets/ticketSlice"
 import Loader from "../components/Loader"
 import BackButton from "../components/BackButton"
+import TicketItem from "../components/TicketItem"
 
 const Tickets = () => {
-  const { tickets, isLoading, isSuccess } = useSelector((state)=>state.tickets)
+  const { tickets, isLoading, isSuccess } = useSelector((state) => state.tickets)
 
   const dispatch = useDispatch()
 
-  useEffect(()=>{
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset())
+      }
+    }
+  }, [dispatch, isSuccess])
+
+  useEffect(() => {
     dispatch(getTickets())
-  },[dispatch])
+  }, [dispatch])
 
   if (isLoading) {
     return <Loader />
   }
 
   return (
-    <div>Tickets</div>
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   )
 }
 
