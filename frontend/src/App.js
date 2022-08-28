d69ca8687062145fc194fae4e683e9cb6b011dcc
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-28 15:34:00 +0930

    add private route New Ticket via custom hook (useAuthStatus) and nested in PrivateRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,24 @@ import Home from "./pages/Home"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Header from "./components/Header"
+import NewTicket from "./pages/NewTicket"
 import { ToastContainer } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css'
+import "react-toastify/dist/ReactToastify.css"
+import PrivateRoute from "./components/PrivateRoute"
 
 const App = () => {
   return (
     <>
       <Router>
         <div className="container">
-        <Header />
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
