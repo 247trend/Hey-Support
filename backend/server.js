8155022f78fd2326f9436a46435221abc8fac969
Jason Yuan <jasonyuan0130@gmail.com>
2022-08-30 20:38:37 +0930

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require("colors")
 const PORT = process.env.PORT || 5050
 const { errorHandler } = require("./middleware/errorMiddleware")
 const connectDB = require("./config/db")
-
+const path = require("path")
 
 //Connect to database
 connectDB()
@@ -14,13 +14,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Hey Support API" })
-})
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes"))
 app.use("/api/tickets", require("./routes/ticketRoutes"))
+
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  //Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+  app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"))
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to Hey Support API" })
+  })
+}
 
 app.use(errorHandler)
 
